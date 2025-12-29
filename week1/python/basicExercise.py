61e63c702389fe70bca5addaf95dfe6ae6dc89ed
Prasad Ranasinghe <janendrar@gmail.com>
2025-12-29 19:32:37 +0100

    update basic python exercise with remove comment codes

--- week1/python/basicExercise.py
+++ week1/python/basicExercise.py
@@ -1,12 +1,12 @@
-# #Exercise 1
-# weight = float(input("Enter your weight: "))
-# unit = input("(K)g or (L)bs: ")
-# if unit.upper() == "K":
-#     convert = weight / 0.45
-#     print("weight in lbs: " + str(convert))
-# else:
-#     convert = weight * 0.45
-#     print("weight in kg: " + str(convert))
+#Exercise 1
+weight = float(input("Enter your weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit.upper() == "K":
+    convert = weight / 0.45
+    print("weight in lbs: " + str(convert))
+else:
+    convert = weight * 0.45
+    print("weight in kg: " + str(convert))
 
 
 #Reverse a String
